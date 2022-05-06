3100c6c0609ed34739ded8eed3db4f4ab46eb9b7
Justin Knox <techbiotic@posteo.de>
2022-05-06 02:07:07 -0400

    added port scan range to the python script

--- rtfm_base/port_scan_range.py
+++ rtfm_base/port_scan_range.py
@@ -1,8 +1,11 @@
 import socket as sk
 ip = input("Enter ipv4 addr (nnn.nnn.nnn.nnn) to scan: ")
-port_low = ("Enter port low / to begin from: ")
-port_high = ("Enter port high / to end from: ")
-for port in range (port_low,port_high):
+port_low = int(input("Enter port low / to begin from: "))
+port_high = int(input("Enter port high / to end from: "))
+port_range = port_high - port_low
+
+for port in range(port_range):
+    print('...%d' %(port))      
     try:
         s=sk.socket(sk.AF_INET,sk.SOCK_STREAM)
         s.settimeout(1000)
