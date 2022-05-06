5bd239c9bd97145b01436c037201f53fa37a29e4
Justin Knox <techbiotic@posteo.de>
2022-05-06 03:13:41 -0400

    refactored, removed comments

--- rtfm_base/port_scanners/port_scanner_random.py
+++ rtfm_base/port_scanners/port_scanner_random.py
@@ -1,5 +1,6 @@
 import socket as sk
 import random
+
 ip = input("Enter ipv4 addr (nnn.nnn.nnn.nnn) to scan: ")
 port_low = int(input("Enter port low / to begin from: "))
 port_high = int(input("Enter port high / to end from: "))
@@ -7,11 +8,7 @@ port_range = port_high - port_low
 offset_low = int(input("offset low (ms): "))
 offset_high = int(input("offset high (ms): "))
 
-print(random.randrange(1,10))
-
-
 for port in range(port_range):   
-#    print('trying port..%d' + str(port) + 'at ' + str(ip))
     offset = random.randrange(offset_low,offset_high)
     print("trying port {port} at {ip} on offset {offset}".format(port=port, ip=ip, offset=offset))
     try:
