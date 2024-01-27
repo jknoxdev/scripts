f413c4a2b58d8239eca0a3d7759fefaea32a52bb
jknoxdev <techbiotic@posteo.de>
2024-01-27 14:32:46 -0500

    --refresh--2024w046.1432

--- push_repos.sh
+++ push_repos.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd ./notes              ; git add .; git commit -m "--refresh--$(date -I)"; git push; cd ../;
-cd ./scripts		; git add .; git commit -m "--refresh--$(date -I)"; git push; cd ../;
-cd ./n3s		; git add .; git commit -m "--refresh--$(date -I)"; git push; cd ../;
-cd ./video-lan-checker	; git add .; git commit -m "--refresh--$(date -I)"; git push; cd ../;
-cd ./jknoxdev		; git add .; git commit -m "--refresh--$(date -I)"; git push; cd ../;
-cd ./blog		; git add .; git commit -m "--refresh--$(date -I)"; git push; cd ../;
-cd ./coursework		; git add .; git commit -m "--refresh--$(date -I)"; git push; cd ../;
-cd ./hack		; git add .; git commit -m "--refresh--$(date -I)"; git push; cd ../;
-cd ./tools		; git add .; git commit -m "--refresh--$(date -I)"; git push; cd ../;
-cd ./ctf-tools		; git add .; git commit -m "--refresh--$(date -I)"; git push; cd ../;
+cd ~/code/jknoxdev/notes             ; git add .; git commit -m "--refresh--$(date '+%Yw%V%w.%H%M')"; git push;
+cd ~/code/jknoxdev/scripts	     ; git add .; git commit -m "--refresh--$(date '+%Yw%V%w.%H%M')"; git push;
+cd ~/code/jknoxdev/n3s		     ; git add .; git commit -m "--refresh--$(date '+%Yw%V%w.%H%M')"; git push;
+cd ~/code/jknoxdev/video-lan-checker ; git add .; git commit -m "--refresh--$(date '+%Yw%V%w.%H%M')"; git push;
+cd ~/code/jknoxdev/jknoxdev	     ; git add .; git commit -m "--refresh--$(date '+%Yw%V%w.%H%M')"; git push;
+cd ~/code/jknoxdev/blog		     ; git add .; git commit -m "--refresh--$(date '+%Yw%V%w.%H%M')"; git push;
+cd ~/code/jknoxdev/coursework	     ; git add .; git commit -m "--refresh--$(date '+%Yw%V%w.%H%M')"; git push;
+cd ~/code/jknoxdev/hack		     ; git add .; git commit -m "--refresh--$(date '+%Yw%V%w.%H%M')"; git push;
+cd ~/code/jknoxdev/tools	     ; git add .; git commit -m "--refresh--$(date '+%Yw%V%w.%H%M')"; git push;
+cd ~/code/jknoxdev/ctf-tools	     ; git add .; git commit -m "--refresh--$(date '+%Yw%V%w.%H%M')"; git push;
 
 echo 'done!'
