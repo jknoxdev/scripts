0708fa4ac641cfa069ecde90ac2b828175fb9c4d
Justin Knox <techbiotic@posteo.de>
2024-02-09 05:03:02 -0500

    --rzb16wl--2024w06.5.0503--

--- push_repos.sh
+++ push_repos.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd ~/code/jknoxdev/notes             ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V%w.%H%M')--"; git push;
-cd ~/code/jknoxdev/scripts	     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V%w.%H%M')--"; git push;
-cd ~/code/jknoxdev/n3s		     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V%w.%H%M')--"; git push;
-cd ~/code/jknoxdev/video-lan-checker ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V%w.%H%M')--"; git push;
-cd ~/code/jknoxdev/jknoxdev	     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V%w.%H%M')--"; git push;
-cd ~/code/jknoxdev/blog		     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V%w.%H%M')--"; git push;
-cd ~/code/jknoxdev/coursework	     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V%w.%H%M')--"; git push;
-cd ~/code/jknoxdev/hack		     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V%w.%H%M')--"; git push;
-cd ~/code/jknoxdev/tools	     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V%w.%H%M')--"; git push;
-cd ~/code/jknoxdev/ctf-tools	     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V%w.%H%M')--"; git push;
+cd ~/code/jknoxdev/notes             ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V.%w.%H%M')--"; git push;
+cd ~/code/jknoxdev/scripts	     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V.%w.%H%M')--"; git push;
+cd ~/code/jknoxdev/n3s		     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V.%w.%H%M')--"; git push;
+cd ~/code/jknoxdev/video-lan-checker ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V.%w.%H%M')--"; git push;
+cd ~/code/jknoxdev/jknoxdev	     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V.%w.%H%M')--"; git push;
+cd ~/code/jknoxdev/blog		     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V.%w.%H%M')--"; git push;
+cd ~/code/jknoxdev/coursework	     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V.%w.%H%M')--"; git push;
+cd ~/code/jknoxdev/hack		     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V.%w.%H%M')--"; git push;
+cd ~/code/jknoxdev/tools	     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V.%w.%H%M')--"; git push;
+cd ~/code/jknoxdev/ctf-tools	     ; git add .; git commit -m "--$(echo $(hostname))--$(date '+%Yw%V.%w.%H%M')--"; git push;
 
 echo 'done!'
