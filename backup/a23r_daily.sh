21bef878158acc604acd1ce65334ae2cd2deeeeb
jknoxdev <techbiotic@posteo.de>
2024-03-29 11:11:15 +0000

    --a23r--20241351111--

--- backup/a23r_daily.sh
+++ backup/a23r_daily.sh
@@ -4,4 +4,4 @@
 echo "----------backing up----------"
 
 cd /net/d7omv/export/d7omvfs/backups/a23r/
-tar -cvzf ./homedir_$(date +%Yw%Wd%w).lzop ~/
+tar -cvzf ./homedir_$(date +%Yw%Wd%w%Z%H%m).lzop ~/
