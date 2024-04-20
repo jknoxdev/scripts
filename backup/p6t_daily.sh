fffd557e2a773d28677e121f0b3bc31fe449ce1a
jknoxdev <techbiotic@posteo.de>
2024-04-19 23:12:26 -0400

    --p6t--20241652312--

--- backup/p6t_daily.sh
+++ backup/p6t_daily.sh
@@ -5,5 +5,27 @@ echo "----------backing up----------"
 # autofs
 # cd /net/d7omv/export/d7omvfs/backups/a23t/
 # manual fstab
-cd /mnt/nfs/d7omv/d7omvfs/backups/a23t
-tar -cvzf ./homedir_$(date +%Yw%Wd%wh%H%m).lzop ~/
+# cd /mnt/nfs/d7omv/d7omvfs/backups/a23t 
+
+# cd ~/
+# echo "tar -cvzf ./homedir_$(date +%Yw%Wd%wh%H%m).lzop ~/"
+
+
+
+# trying out both ways.. first we will try pull from server
+# https://www.cyberciti.biz/faq/howto-use-tar-command-through-network-over-ssh-session/
+# add to crontab -e after testing
+
+# /usr/bin/ssh arx@p6t "sudo tar -cf - -C /home/arx/ | gzip -9c" > "/mnt/data/arx/fs/60-69 Projects/66 Homelab/66.31 w541_p6t_backup/p6t_arx_homedir_$(date +\%Y\%m%dt%H\%M).tar.gz"
+
+# i don't like this, getting stuck cause I don't want to open up root perms...
+
+# method 2
+# pipe over ssh
+# http://www.linux-admins.net/2010/09/backing-up-with-tar-over-ssh.html
+
+# tar zcvf - /home | ssh server "cat > inky-homes.tgz"
+
+# tar czvf - /home/arx/ | ssh tnas23 "cd /mnt/data/arx/fs/60-69 Projects/66 Homelab/66.31 w541_p6t_backup/ ; echo "p6t_arx_homedir_$(date +\%Y\%m%dt%H\%M).tgz"; pwd"
+
+sudo tar -zcvf - /home/arx/ | ssh tnas23 "cd '/mnt/data/arx/fs/60-69 Projects/66 Homelab/66.31 w541_p6t_backup/' ; cat > "p6t_arx_homedir_$(date +\%Y\%m%dt%H\%M).tgz" " 
